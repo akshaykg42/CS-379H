712a5dc39613d486aef757c07297abcec40831dc
akshaykg42 <akshaykg42@gmail.com>
2020-02-03 17:17:43 -0600

    added init

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import nltk.data
 import re
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+nltk.download('punkt')
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 def preprocess_sentence(sentence):
