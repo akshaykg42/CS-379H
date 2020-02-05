83f0207d8b5329c48388d1fe6a57f5b9d2c359cb
akshaykg42 <akshaykg42@gmail.com>
2020-02-04 19:55:13 -0600

    added code for comparing model outputs to oracle

--- main.py
+++ main.py
@@ -2,13 +2,38 @@ from utils import *
 from model import *
 from train import *
 from test import *
+import math
 from sklearn.model_selection import train_test_split
 
 sent_type = 0
 
 if __name__ == '__main__':
 	print('Loading data...')
-	X, y = load('pcr_documents.pkl', 'pcr_summaries.pkl', 'pcr_oracles.pkl', sent_type)
+	documents, summaries, oracles = load('pcr_documents.pkl', 'pcr_summaries.pkl', 'pcr_oracles.pkl')
+	X, y = get_features_and_labels(documents, summaries, oracles, sent_type)
 	X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(X, y, np.arange(len(X)))
 	train(X_train, y_train)
-	test(X_test, y_test)
+	test_scores = test(X_test, y_test).numpy()
+
+	for i, index in enumerate(indices_test):
+		document = documents[index]
+		summary = summaries[index]
+		oracle_index = oracles[index][sent_type]
+		model_scores = test_scores[i]
+		document_sentences = tokenizer.tokenize(document)
+		summary_sentences = tokenizer.tokenize(summary)
+		ground_truth_sentence = summary[sent_type]
+		oracle_sentence = document_sentences[oracle_index]
+		model_best_sentence_indices = model_scores.flatten().argsort()[-5:][::-1]
+		model_best_scores = np.sort(model_scores.flatten())[-5:][::-1]
+		model_best_probabilities = [math.exp(score) for score in model_best_scores]
+		model_best_sentences = [document_sentences[j] for j in model_best_sentences]
+		oracle_rouge = get_rouge(oracle_sentence, ground_truth_sentence, '2', 'f')
+		print('Oracle Sentence rouge({}):'.format(oracle_rouge))
+		print(oracle_sentence)
+		for model_sentence, model_probability in zip(model_best_sentences, model_best_probabilities):
+			model_rouge = get_rouge(model_sentence, ground_truth_sentence, '2', 'f')
+			print('Model Sentence p[{}] rouge({}):'.format(model_probability, model_rouge))
+			print(model_sentence)
+		print('----------------------------------------------------------------------')
+
--- utils.py
+++ utils.py
@@ -9,8 +9,14 @@ import numpy as np
 import torch
 from sklearn.feature_extraction.text import CountVectorizer
 from itertools import accumulate
+from rouge import Rouge
+rouge = Rouge()
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 stopwords = ["a", "about", "above", "after", "again", "against", "ain", "all", "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do", "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has", "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her", "here", "hers", "herself", "him", "himself", "his", "how", "i", "if", "in", "into", "is", "isn", "isn't", "it", "it's", "its", "itself", "just", "ll", "m", "ma", "me", "mightn", "mightn't", "more", "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no", "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s", "same", "shan", "shan't", "she", "she's", "should", "should've", "shouldn", "shouldn't", "so", "some", "such", "t", "than", "that", "that'll", "the", "their", "theirs", "them", "themselves", "then", "there", "these", "they", "this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we", "were", "weren", "weren't", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "won", "won't", "wouldn", "wouldn't", "y", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves", "could", "he'd", "he'll", "he's", "here's", "how's", "i'd", "i'll", "i'm", "i've", "let's", "ought", "she'd", "she'll", "that's", "there's", "they'd", "they'll", "they're", "they've", "we'd", "we'll", "we're", "we've", "what's", "when's", "where's", "who's", "why's", "would"]
+
+def get_rouge(hypothesis, reference, rougetype, scoretype):
+	rougetype = 'rouge-' + rougetype
+	return rouge.get_scores(hypothesis, reference)[0][rougetype][scoretype]
 
 def pad_and_mask(batch_inputs):
 	batch_size = len(batch_inputs)
@@ -91,22 +97,14 @@ def get_features_and_labels(pcr_documents, pcr_summaries, pcr_oracles, type):
 	y = np.array(y)
 	return X, y
 
-def load(documents, summaries, oracle_indices, sent_type):
-	
-	"""
-	Loads the data that is provided
-	@param filename: The name of the data file. Can be either 'tux_train.dat' or 'tux_val.dat'
-	@return images: Numpy array of all images where the shape of each image will be W*H*3
-	@return labels: Array of integer labels for each corresponding image in images
-	"""
-
+def load(documents, summaries, oracle_indices):
 	with open(documents, 'rb') as f:
-		pcr_documents = pickle.load(f)
+		documents = pickle.load(f)
 
 	with open(summaries, 'rb') as f:
-		pcr_summaries = pickle.load(f)
+		summaries = pickle.load(f)
 
 	with open(oracle_indices, 'rb') as f:
-		pcr_oracles = pickle.load(f)
+		oracles = pickle.load(f)
 
-	return get_features_and_labels(pcr_documents, pcr_summaries, pcr_oracles, sent_type)
+	return documents, summaries, oracles
