9eff43442cda75bb2e4e8e3e753611c089210f5c
akshaykg42 <akshaykg42@gmail.com>
2020-02-10 05:55:25 -0600

    printing stuff

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ if __name__ == '__main__':
 	summaries = [summaries[i] for i in range(len(summaries)) if i in available_indices]
 	oracles = [oracles[i] for i in range(len(oracles)) if i in available_indices]
 
+	oracle_rouges = []
+	model_rouges = []
 	for i, index in enumerate(indices_test):
 		document = documents[index]
 		summary = summaries[index]
@@ -40,15 +42,26 @@ if __name__ == '__main__':
 		model_best_probabilities = [math.exp(score) for score in model_best_scores]
 		model_best_sentences = [document_sentences[j] for j in model_best_sentence_indices]
 		oracle_rouge = get_rouge(oracle_sentence, ground_truth_sentence, '1', 'f')
+		oracle_rouges.append(oracle_rouge)
 		print('Oracle Sentence rouge({}):'.format(oracle_rouge))
 		print(oracle_sentence)
 		print()
+		tmp = []
 		for model_sentence, model_probability in zip(model_best_sentences, model_best_probabilities):
 			model_rouge = get_rouge(model_sentence, ground_truth_sentence, '1', 'f')
+			tmp.append(model_rouge)
 			print('Model Sentence p[{}] rouge({}):'.format(model_probability, model_rouge))
 			print(model_sentence)
 			print()
+		model_rouges.append(tmp)
 		if(oracle_sentence in model_best_sentences):
 			print('ORACLE IN TOP 5')
 		print('----------------------------------------------------------------------\n')
+	avg_model_rouges = [np.mean(i) for i in model_rouges]
+	min_model_rouges = [min(i) for i in model_rouges]
+	max_model_rouges = [max(i) for i in model_rouges]
+	print(oracle_rouges)
+	print(avg_model_rouges)
+	print(min_model_rouges)
+	print(max_model_rouges)
 
